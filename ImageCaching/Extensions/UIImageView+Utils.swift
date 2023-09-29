be4f6428b5a687b8113af38f0df35fd053667c2f
Marcelo Sampaio <marcelo.sampaio@premiersoft.net>
2023-09-29 11:22:27 -0300

    🔥 Version 1.0 🔥

--- ImageCaching/Extensions/UIImageView+Utils.swift
+++ ImageCaching/Extensions/UIImageView+Utils.swift
@@ -1,21 +1,12 @@
-//
-//  UIImageView+Utils.swift
-//  ImageCaching
-//
-//  Created by Premiersoft on 28/09/23.
-//
-
 import UIKit
 
 extension UIImageView {
     func setImage(url: URL) {
         ImageCacheManager.downloadImage(url: url) { image, error in
-            DispatchQueue.main.async {
-                if (error != nil) {
-                    self.image = UIImage()
-                } else {
-                    self.image = image
-                }
+            if (error != nil) {
+                self.image = UIImage()
+            } else {
+                self.image = image
             }
         }
     }
--- ImageCaching/Image Cache Manager/ImageCacheManager.swift
+++ ImageCaching/Image Cache Manager/ImageCacheManager.swift
@@ -1,23 +1,7 @@
-//
-//  ImageCacheManager.swift
-//  ImageCaching
-//
-//  Created by Premiersoft on 28/09/23.
-//
 import UIKit
 
-typealias JSON = [String : Any]
 fileprivate let imageCache = NSCache<NSString, UIImage>()
-
-extension NSError {
-    static func generalParsingError(domain: String) -> Error {
-        return NSError(domain: domain, code: 400, userInfo: [NSLocalizedDescriptionKey : NSLocalizedString("ImageCacheManager error retrieving data", comment: "General Parsing Error Description")])
-    }
-}
-
 class ImageCacheManager {
-    
-    //MARK: - Public
     static func downloadImage(url: URL, completion: @escaping (_ image: UIImage?, _ error: Error? ) -> Void) {
         if let cachedImage = imageCache.object(forKey: url.absoluteString as NSString) {
             completion(cachedImage, nil)
@@ -25,46 +9,25 @@ class ImageCacheManager {
             ImageCacheManager.downloadData(url: url) { data, response, error in
                 if let error = error {
                     completion(nil, error)
-                    
                 } else if let data = data, let image = UIImage(data: data) {
                     imageCache.setObject(image, forKey: url.absoluteString as NSString)
                     completion(image, nil)
                 } else {
-                    completion(nil, NSError.generalParsingError(domain: url.absoluteString))
+                    completion(nil, NSError.imageCacheProcessError(domain: url.absoluteString))
                 }
             }
         }
     }
-    
-    static func search(for query: String, page: Int, completion: @escaping (_ responseObject: [String : Any]?, _ error: Error?) -> Void) {
-        if let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
-            let url = URL(string:"http://www.omdbapi.com/?s=\(encodedQuery)&page=\(page)") {
-            ImageCacheManager.downloadData(url: url) { data, response, error in
-                if let error = error {
-                    completion(nil, error)
-                } else {
-                    if let data = data, let responseObject = self.convertToJSON(with: data) {
-                        completion(responseObject, nil)
-                    } else {
-                        completion(nil, NSError.generalParsingError(domain: url.absoluteString))
-                    }
-                }
-            }
-        }
-    }
-    
-    //MARK: - Private
+
     fileprivate static func downloadData(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
         URLSession(configuration: .ephemeral).dataTask(with: URLRequest(url: url)) { data, response, error in
             completion(data, response, error)
             }.resume()
     }
-    
-    fileprivate static func convertToJSON(with data: Data) -> JSON? {
-        do {
-            return try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? JSON
-        } catch {
-            return nil
-        }
+}
+
+extension NSError {
+    static func imageCacheProcessError(domain: String) -> Error {
+        return NSError(domain: domain, code: 400, userInfo: [NSLocalizedDescriptionKey : NSLocalizedString("Erro ao processar imagem", comment: "Erro ao processar imagem")])
     }
 }
